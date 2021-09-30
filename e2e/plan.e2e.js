93fc769597e9cfc60453f1550931a0577bb5a07e
Charles Pereira <charlesti.ptu@gmail.com>
2021-09-29 23:34:17 -0300

    test e2e automated

--- e2e/plan.e2e.js
+++ e2e/plan.e2e.js
@@ -5,13 +5,34 @@ describe("Example", () => {
   });
 
   //Executa antes de cada test
-  beforeEach(async () => {
-    await device.reloadReactNative();
-  });
+  // beforeEach(async () => {
+  //   await device.reloadReactNative();
+  // });
 
   it("should have info about cancel subscription", async () => {
     const info = await element(by.text("Cancel anytime. Ofter terms apply."));
 
     await expect(info).toBeVisible();
+  });
+
+  it("shoud subscribe to them premium plan", async () => {
+    const clickButton = element(by.id("confirmation-message"));
+
+    await element(by.id("option-premium")).tap();
+
+    await element(by.id("input-email")).tap();
+
+    await element(by.id("input-email")).typeText("charlesti.ptu@gmail.com");
+
+    await new Promise((t) => setTimeout(t, 2000));
+
+    await element(by.id("keyboard")).tap();
+
+    await new Promise((t) => setTimeout(t, 3000));
+
+    await element(by.id("button-subscribe")).tap();
+
+    await expect(clickButton).toBeVisible();
+    await new Promise((t) => setTimeout(t, 3000));
   });
 });
